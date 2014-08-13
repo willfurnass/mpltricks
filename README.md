8dd98fa67f76e75ed861414078bde26c1d8e1728
Will Furnass <will@thearete.co.uk>
2014-08-13 14:38:38 +0100

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 mpltricks
 =========
 
-Matplotlib helper functions
+### tick\_skip(ax, keep\_every=2, x\_or\_y='y', min\_cur_\lines=4)
+
+Reduce the number of ticks on a given matplotlib Axes object
+
+ * ax -- matplotlib.axes.Axes object
+ * keep\_every -- keep one in this many ticks e.g. set to 3 to reduce the number of ticks by 2/3
+ * x\_or\_y -- can be 'x' or 'y'; reduces the number of ticks on the corresponding axis
+ * min\_cur\_lines -- only reduce the number of ticks if the total number to start with is more than this
+
+### line\_style\_cycle(ax, line\_styles=('-', '--',  '-.', ':'))
+
+Update the style of all lines in a matplotlib Axes in a round-robin
+fashion. If line\_styles contains four elements but ax has five Lines associated with
+it then the first and fifth lines will have the same style.
+
+ * ax -- matplotlib.axes.Axes object
+ * line\_styles -- the list of matplotlib line styles that are cycled through
+
+### subplot\_ids(axes, x\_rel=0.9, y\_rel=0.9)
+
+Annotate each of several matplotlib Axes with an alpha identifier.  e.g. Draw an '(a)' label on the first Axes object, a '(b)' on the second etc.
+
+ * axes -- An enumerable collection of matplotlib.axes.Axes objects
+ * x\_rel -- Relative position of the alpha identifier w.r.t. the x axis
+ * y\_rel -- Relative position of the alpha identifier w.r.t. the y axis
---
+++ mpltricks/mpltricks.py
@@ -0,0 +1,65 @@
+from __future__ import absolute_import
+from itertools import cycle
+import string
+
+
+def tick_skip(ax, keep_every=2, x_or_y='y', min_cur_lines=4):
+    """Reduce the number of ticks on a given matplotlib Axes object
+
+    Arguments:
+    ax -- matplotlib.axes.Axes object
+    keep_every -- keep one in this many ticks e.g. set to 3 to
+                  reduce the number of ticks by 2/3
+    x_or_y -- can be 'x' or 'y'; reduces the number of ticks on the
+              corresponding axis
+    min_cur_lines -- only reduce the number of ticks if the total
+                     number to start with is more than this
+
+    """
+    if x_or_y == 'y':
+        tick_setter = ax.set_yticks
+        cur_ticks = ax.get_yticks()
+    elif x_or_y == 'x':
+        tick_setter = ax.set_xticks
+        cur_ticks = ax.get_xticks()
+    else:
+        raise Exception("x_or_y must be 'x' or 'y'")
+
+    if len(cur_ticks) > min_cur_lines:
+        tick_setter([tick for tick_idx, tick in enumerate(cur_ticks)
+                     if tick_idx % keep_every == 0])
+
+
+def line_style_cycle(ax, line_styles=('-', '--',  '-.', ':')):
+    """Update the style of all lines in a matplotlib Axes in a round-robin
+    fashion.
+
+    If line_styles contains four elements but ax has five Lines associated with
+    it then the first and fifth lines will have the same style.
+
+    Arguments:
+    ax -- matplotlib.axes.Axes object
+    line_styles -- the list of matplotlib line styles that are cycled through
+
+    """
+    for line, line_style in zip(ax.get_lines(), cycle(line_styles)):
+        line.set_linestyle(line_style)
+
+
+def subplot_ids(axes, x_rel=0.9, y_rel=0.9):
+    """Annotate each of several matplotlib Axes with an alpha identifier.
+
+    e.g. Draw an '(a)' label on the first Axes object, a '(b)' on the
+    second etc.
+
+    Arguments:
+    axes -- An enumerable collection of matplotlib.axes.Axes objects
+    x_rel -- Relative position of the alpha identifier w.r.t. the x axis
+    y_rel -- Relative position of the alpha identifier w.r.t. the y axis
+
+    """
+    for ax, _id in zip(axes, cycle(string.ascii_lowercase)):
+        x = ((ax.get_xlim()[1] - ax.get_xlim()[0]) * x_rel) + ax.get_xlim()[0]
+        y = ((ax.get_ylim()[1] - ax.get_ylim()[0]) * y_rel) + ax.get_ylim()[0]
+        ax.text(x, y, "({})".format(_id),
+                bbox=dict(facecolor='white', alpha=0.5, edgecolor='white'))
